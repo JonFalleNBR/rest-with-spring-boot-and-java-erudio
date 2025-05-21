29b04d6efc0a2382408f9715edd442606ca67838
Jonathan-Silva <91348218+JonFalleNBR@users.noreply.github.com>
2025-05-20 21:47:46 -0300

    feat-commit_inicial

---
+++ rest-spring-rest-java/rest-spring-rest-java/src/main/java/br/com/jonfallenbr/rest_spring_rest_java/RestSpringRestJavaApplication.java
@@ -0,0 +1,15 @@
+package br.com.jonfallenbr.rest_spring_rest_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSpringRestJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestSpringRestJavaApplication.class, args);
+	}
+
+	//System.ou  ("Ola Mundo");
+
+}
---
+++ rest-spring-rest-java/rest-spring-rest-java/src/test/java/br/com/jonfallenbr/rest_spring_rest_java/RestSpringRestJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jonfallenbr.rest_spring_rest_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSpringRestJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
